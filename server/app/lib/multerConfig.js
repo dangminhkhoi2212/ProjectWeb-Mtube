7925b4222b2b3afe1c8a4a3be1b1d5353779de8f
dangminhkhoi2212 <kkhoi600@gmail.com>
2023-07-01 19:54:50 +0700

    update middleware

--- server/app/lib/multerConfig.js
+++ server/app/lib/multerConfig.js
@@ -2,26 +2,31 @@ const multer = require('multer');
 const path = require('path');
 const ApiError = require('../api.error');
 const maxSize = 10 * 1024 * 1024;
+
 // Multer config
-module.exports = multer({
-    storage: multer.diskStorage({}),
-    limits: { fileSize: maxSize, fieldSize: maxSize },
-    fileFilter: (req, file, cb) => {
-        let ext = path.extname(file.originalname);
-        if (
-            ext !== '.mp4' &&
-            ext !== '.mkv' &&
-            ext !== '.jpeg' &&
-            ext !== '.jpg' &&
-            ext !== '.png' &&
-            ext !== '.gif' &&
-            ext !== '.pdf' &&
-            ext !== '.docx' &&
-            ext !== '.doc'
-        ) {
-            cb(new ApiError(403, 'File type is not supported'), false);
-            return;
-        }
-        cb(null, true);
-    },
+const storage = multer.diskStorage({});
+const fileFilter = (req, file, cb) => {
+    let ext = path.extname(file.originalname);
+    if (
+        ext !== '.mp4' &&
+        ext !== '.mkv' &&
+        ext !== '.jpeg' &&
+        ext !== '.jpg' &&
+        ext !== '.png' &&
+        ext !== '.gif' &&
+        ext !== '.pdf' &&
+        ext !== '.docx' &&
+        ext !== '.doc'
+    ) {
+        return cb(new ApiError(403, 'File type is not supported'), false);
+    }
+    cb(null, true);
+};
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: maxSize },
+    fileFilter: fileFilter,
 });
+
+module.exports = upload;
